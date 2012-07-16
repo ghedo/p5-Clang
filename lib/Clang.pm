c8273bb076c758a94bbf8bf1a315afd022f099f2
Alessandro Ghedini <al3xbio@gmail.com>
2012-07-16 11:53:12 +0200

    Clang.pm: a little better documentation

--- lib/Clang.pm
+++ lib/Clang.pm
@@ -8,7 +8,7 @@ XSLoader::load('Clang', $Clang::VERSION);
 
 =head1 NAME
 
-Clang - Perl bindings to the Clang compiler's programming interface
+Clang - Perl bindings to the Clang compiler's indexing interface
 
 =head1 SYNOPSIS
 
@@ -17,11 +17,11 @@ Clang - Perl bindings to the Clang compiler's programming interface
     my $index = Clang::Index -> new(1);
 
     my $tunit = $index -> parse('file.c');
-    my @cursors = $tunit -> cursor -> children;
+    my $nodes = $tunit -> cursor -> children;
 
-    foreach my $cursor (@cursors) {
-        say $cursor -> spelling;
-        say $cursor -> kind -> spelling;
+    foreach my $node (@$nodes) {
+        say $node -> spelling;
+        say $node -> kind -> spelling;
     }
 
 =head1 DESCRIPTION
@@ -29,12 +29,9 @@ Clang - Perl bindings to the Clang compiler's programming interface
 Clang is a compiler front end for the C, C++, Objective-C, and Objective-C++
 programming languages which uses LLVM as its back end.
 
-B<Clang::Index> provides Perl bindings to the Clang indexing interface, used
-for extracting high-level symbol information from source files without exposing
+This module provides Perl bindings to the Clang indexing interface, used for
+extracting high-level symbol information from source files without exposing
 the full Clang C++ API.
-
-B<Attention>: note that this module is still under development and currently
-lacks most of the functionality needed to make it useful.
 
 =head1 AUTHOR
 
